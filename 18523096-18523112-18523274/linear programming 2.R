568acd834489633115b0dd75932c5f39efc33208
ChilyaChumaira <58930530+ChilyaChumaira@users.noreply.github.com>
2019-12-16 13:57:11 +0700

    Add files via upload

---
+++ 18523096-18523112-18523274/linear programming 2.R
@@ -0,0 +1,11 @@
+library(lpSolve)
+
+f.obj <- c(8000,12000)
+f.con <- matrix(c(3,4,
+                  1,3),nrow=2,byrow=TRUE)
+f.dir <-c("<=",
+          "<=")
+f.rhs <- c(60,
+           30)
+lp("max",f.obj,f.con,f.dir,f.rhs)
+lp("max",f.obj,f.con,f.dir,f.rhs)$solution
